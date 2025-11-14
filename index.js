9f518c7fae7abf0e4f11e2ce8b90225a95a083e1
Bindusha Bavurupudi <bindushabavurupudi2006@gmail.com>
2025-11-14 11:08:06 +0530

    added login function

--- index.js
+++ index.js
@@ -8,3 +8,17 @@ function signup(userName){
         return "Signup Successfull, Please Login";
     }
 }
+
+function login(userName, password){
+    let usernames = ["Bindusha", "Ritika", "Pranathi", "Navya", "Pravalika", "Uhanjani"];
+    for(let i=0; i<usernames.length; i++){
+        if(usernames[i] === userName && password === "Emp@123"){
+            return "Login Successful...";
+        }
+        else if(usernames[i] === userName && password !== "Emp@123"){
+            return "Wrong Password....";
+        }
+    }
+    return "User Not Found, Please Signup";
+
+}
